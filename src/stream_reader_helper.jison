2d4d3421baf002d538f20b443eee00d691c48bb3
Adam Koch <adam@dogtired.org>
2021-10-07 00:49:50 -0500

    better save this

---
+++ src/stream_reader_helper.jison
@@ -0,0 +1,158 @@
+/* simple parser */
+
+/* lexical grammar */
+%lex
+
+word         [a-zA-Z]+\b
+number       [0-9]+
+space			[\t \u00a0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000]
+other        [^a-zA-Z0-9 \n]+ 
+
+
+%s ATTRS
+%x TEXT_START
+%%
+
+<INITIAL>\/\/\s* %{
+  this.pushState('TEXT_START')
+  return 'COMMENT'
+%}
+<INITIAL>'<'[A-Z_]+'>'  %{
+  this.pushState(yytext.substring(1, yytext.length - 1));
+%}
+<INITIAL>^"doctype html" return 'DOCTYPE';
+<INITIAL>(mixin|include|block)        %{
+  this.pushState('ATTRS')
+  return 'DIRECTIVE';
+%}
+
+<INITIAL>{word}       return 'TAG_NAME';
+<INITIAL>{number}     return 'NUMBER';
+<INITIAL>'('             %{
+  this.pushState('ATTRS')
+  return 'LPAREN'
+%}
+<INITIAL>')'             return 'RPAREN';
+'| '              %{
+  this.pushState('ATTRS')
+  return 'TEXT_START'
+%}
+'.'   return 'DOT'
+<INITIAL>{other}      return 'OTHER';
+{space}      return 'SPACE';
+<INITIAL><<EOF>>      return 'ENDOFFILE';
+<INITIAL>\n           return 'NEWLINE'; // ignore newlines
+<ATTRS>[^)\n]+          %{
+  this.popState()
+  return 'THEREST'
+%}
+<TEXT_START>[^\n\r]+  %{
+  this.popState()
+  return 'TEXT'
+%}
+
+/lex
+
+%ebnf
+
+%% 
+
+
+/* language grammar */
+
+start
+  : ENDOFFILE
+  | DOCTYPE ENDOFFILE
+  {
+    $$ = { type: 'doctype', type: 'html' }
+  }
+  | TAG_NAME ENDOFFILE
+  {
+    $$ = { type: 'tag', name: $TAG_NAME }
+  }
+  | TAG_NAME LPAREN THEREST RPAREN ENDOFFILE
+  {
+    $$ = { type: 'tag', name: $TAG_NAME, attrs: $THEREST }
+  }
+  | DIRECTIVE SPACE THEREST ENDOFFILE
+  {
+    $$ = { type: 'directive', name: $DIRECTIVE, params: $THEREST }
+  }
+  | TEXT_START THEREST ENDOFFILE
+  {
+    $$ = { type: 'text', text: $THEREST }
+  }
+  | TAG_NAME DOT SPACE* ENDOFFILE
+  {
+    $$ = { type: 'tag', name: $TAG_NAME, state: 'text_start' }
+  }
+  | TAG_NAME SPACE* TAG_NAME ENDOFFILE
+  {
+    $$ = { type: 'tag', name: $TAG_NAME1, therest: $TAG_NAME2 }
+  }
+  | TEXT ENDOFFILE
+  {
+    $$ = { type: 'text', text: $TEXT }
+  }
+  | DOT TAG_NAME ENDOFFILE
+  {
+    $$ = { type: 'classname', name: $TAG_NAME }
+  }
+  | COMMENT TEXT* ENDOFFILE
+  {
+    $$ = { type: 'comment' }
+  }
+  ;
+
+%% 
+
+// feature of the GH fork: specify your own main.
+//
+// compile with
+// 
+//      jison -o test.js --main path/to/simple.jison
+//
+// then run
+//
+//      node ./test.js
+//
+// to see the output.
+
+var assert = require("assert");
+
+parser.main = function () {
+
+  function test(input, expected) {
+    console.log(`\nTesting '${input}'...`)
+    var actual = parser.parse(input)
+    console.log(input + ' ==> ', JSON.stringify(actual))
+    assert.deepEqual(actual, expected)
+  }
+
+  test('html', { type: 'tag', name: 'html' })
+
+
+test("doctype html", { type: 'doctype', type: 'html' })
+test("html(lang='en-US')", {"type":"tag","name":"html","attrs":"lang='en-US'"})
+
+test("include something", { type: 'directive', name: 'include', params: 'something' })
+test("block here", { type: 'directive', name: 'block', params: 'here' })
+test("head", { type: 'tag', name: 'head' })
+test("meta(charset='UTF-8')", {"type":"tag","name":"meta","attrs":"charset='UTF-8'"})
+test("meta(name='viewport' content='width=device-width')", { type: 'tag', name: 'meta', attrs: "name='viewport' content='width=device-width'"})
+test("title", {"type":"tag","name":"title"})
+test("| White-space and character 160 | Adam Koch ", {"type":"text","text":"White-space and character 160 | Adam Koch "})
+test("script(async src=\"https://www.googletagmanager.com/gtag/js?id=UA-452464-5\")", {"type":"tag","name":"script","attrs":"async src=\"https://www.googletagmanager.com/gtag/js?id=UA-452464-5\""})
+test("script.  ", {"type":"tag","name":"script","state":"text_start"})
+test("<TEXT_START>window.dataLayer = window.dataLayer || [];   ", { type: 'text', text: 'window.dataLayer = window.dataLayer || [];   ' })
+test("<TEXT_START>gtag('config', 'UA-452464-5');", {"type":"text","text":"gtag('config', 'UA-452464-5');"})
+test("", "")
+test("script test", {"type":"tag","name":"script","therest":"test"})
+test("tag", { type: 'tag', name: 'tag' })
+test(".classname", { type: 'classname', name: 'classname' })
+test("// some text", { type: 'comment' })
+test("// ", { type: 'comment' })
+// test("//", { type: 'comment' })
+
+
+};
--- stream_reader.js
+++ stream_reader.js
@@ -1,10 +1,16 @@
 import fs from 'fs'
-import path from 'path'
+import path, { parse } from 'path'
 import stream from 'stream'
 import util from 'util'
+import deepFilter from 'deep-filter';
 
-String.prototype.quote = function() {
-  return this.replaceAll('"', '\\\"')
+import objectify from 'through2-objectify'
+import concat from 'concat-stream'
+
+import parser from './build/stream_reader_helper.cjs'
+
+String.prototype.quote = function () {
+  return this.replaceAll('\\', '\\\\').replaceAll('"', '\\\"')
 }
 
 
@@ -29,7 +35,7 @@ String.prototype.quote = function() {
 
 const lineTransformer = new stream.Transform({
   transform(chunk, enc, callback) {
-    const matches = chunk.toString().matchAll(/.*\n/g);
+    const matches = chunk.toString().matchAll(/.*(\n|$)/g);
     for (const match of matches) {
       this.push(match[0].toString());
     }
@@ -40,7 +46,7 @@ const lineTransformer = new stream.Transform({
 const blankLineRemoverTransformer = new stream.Transform({
   transform(chunk, enc, callback) {
     const match = chunk.toString().match(/^\s*$/);
-    if (match) { 
+    if (match) {
       debug('blankLineRemoverTransformer', 'removing blank line')
     }
     else {
@@ -88,7 +94,7 @@ const indentTransformer = new stream.Transform({
       let num = match.toString().length
       if (num > this.stack[0]) {
         this.stack.unshift(match.toString().length)
-        this.push('INDENT '  + chunk.toString().trim() + '\n')
+        this.push('INDENT ' + chunk.toString().trim() + '\n')
       }
       else if (num < this.stack[0]) {
         while (num < this.stack[0]) {
@@ -103,7 +109,7 @@ const indentTransformer = new stream.Transform({
     if (this.ended) {
       while (1 < this.stack[0]) {
         this.stack.shift()
-        this.push('DEDENT ' );
+        this.push('DEDENT ');
       }
     }
     callback();
@@ -119,6 +125,8 @@ stream.finished(process.stdin, (err) => {
     indentTransformer.ended = true
     nestingTransformer.ended = true
     simpleNestingTransformer.ended = true
+    removeExtraEmptyElementTransformer.ended = true
+    removeExtraEmptyElementTransformer2.ended = true
     // console.log('Stream is done reading.');
   }
 });
@@ -130,18 +138,18 @@ const tagTransformer = new stream.Transform({
 
     debug('tagtransformer', 'chunk=' + chunk.toString())
 
-    let regex = /(?<INDENT>INDENT )?(?<DEDENT>DEDENT)?(?<name>[a-zA-Z0-9]+)?\b(?<attrs>.*)?/
+    const regex = /(?<INDENT>INDENT )?(?<DEDENT>DEDENT)?(?<name>[a-zA-Z0-9]+)?\b(?<attrs>.*)?/
     // const groups = [...chunk.toString().matchAll(regex)]
     const matches = chunk.toString().match(regex)
 
     if (matches) {
       debug('tagtransformer', 'matches=', matches.groups)
       if (matches.groups.INDENT) {
-        ret.push( ', "children": [{ ' )
+        ret.push(', "children": [{ ')
       }
       // TODO: handle mulitple DEDENTs
       else if (matches.groups.DEDENT) {
-        ret.push( '}] ' )
+        ret.push('}] ')
       }
       else {
         ret.push('}, {')
@@ -149,12 +157,12 @@ const tagTransformer = new stream.Transform({
 
       const tagName = matches.groups.name
       if (tagName && tagName.trim().length > 0) {
-        ret.push( '"name": "' + tagName + '"')
+        ret.push('"name": "' + tagName + '"')
       }
 
       const attrs = matches.groups.attrs
       if (attrs && attrs.trim().length > 0) {
-        ret.push( '"attrs": "' + attrs.quote() + '"' )
+        ret.push('"attrs": "' + attrs.quote() + '"')
       }
 
       // let tagName = groups[0][1]
@@ -184,18 +192,18 @@ const nestingTransformer = new stream.Transform({
 
     debug('nestingTransformer', 'chunk=' + chunk.toString())
 
-    let regex = /(?<INDENT>INDENT )?(?<DEDENT>DEDENT)?(?<text>.*)/
+    const regex = /(?<INDENT>INDENT )?(?<DEDENT>DEDENT)?(?<text>.*)/
     // const groups = [...chunk.toString().matchAll(regex)]
     const matches = chunk.toString().match(regex)
 
     if (matches) {
       debug('nestingTransformer', 'matches=', matches.groups)
       if (matches.groups.INDENT) {
-        ret.push( ', "children": [{ ' )
+        ret.push(', "children": [{ ')
       }
       // TODO: handle mulitple DEDENTs
       else if (matches.groups.DEDENT) {
-        ret.push( '}]}, { ' )
+        ret.push('}]}, { ')
       }
       else {
         if (this.first) {
@@ -209,7 +217,7 @@ const nestingTransformer = new stream.Transform({
 
       const text = matches.groups.text
       if (text && text.trim().length > 0) {
-        ret.push( '"text": "' + text.quote() + '"')
+        ret.push('"text": "' + text.quote() + '"')
       }
 
     }
@@ -240,24 +248,24 @@ const simpleNestingTransformer = new stream.Transform({
 
     debug('simpleNestingTransformer', 'chunk=' + chunk.toString())
 
-    let regex = /(?<INDENT>INDENT )?(?<DEDENT>DEDENT)?(?<text>.*)/
+    const regex = /(?<INDENT>INDENT )?(?<DEDENT>DEDENT)?(?<text>.*)/
     // const groups = [...chunk.toString().matchAll(regex)]
     const matches = chunk.toString().match(regex)
 
     if (matches) {
       debug('simpleNestingTransformer', 'matches=', matches.groups)
       if (matches.groups.INDENT) {
-        ret.push( ', [' )
+        ret.push(', [')
       }
       // TODO: handle mulitple DEDENTs
       else if (matches.groups.DEDENT) {
         if (matches.groups.DEDENT.length > 6) {
           for (let i = 0; i < matches.groups.DEDENT.length / 6; i++) {
-            ret.push( '], ' )
+            ret.push('], ')
           }
         }
         else {
-          ret.push( '], ' )
+          ret.push('], ')
         }
       }
       else {
@@ -271,9 +279,9 @@ const simpleNestingTransformer = new stream.Transform({
       }
 
       const text = matches.groups.text
-      if (text && text.trim().length > 0) {
-        ret.push('"' + text.quote() + '"')
-      }
+      // if (text && text.trim().length > 0) {
+      ret.push('"' + text.quote() + '"')
+      // }
 
     }
     else {
@@ -291,51 +299,387 @@ const simpleNestingTransformer = new stream.Transform({
 })
 simpleNestingTransformer.first = true;
 
-const closerTransformer = new stream.Transform({
+// const closerTransformer = new stream.Transform({
+//   flush(callback) {
+//     this.push(this.tokens)
+//     callback()
+//   },
+//   transform(chunk, enc, callback) {
+//     if (!this.hasOwnProperty('tokens')) {
+//       this.tokens = []
+//     }
+//     chunk.forEach((c) => {
+//       // for (var c of chunk.toString()) {
+//       if (c == '[') {
+//         this.tokens.push(']')
+//       }
+//       else if (c == '{') {
+//         this.tokens.push('}')
+//       }
+//       else if (c == '}' || c == ']') {
+//         this.tokens.pop();
+//       }
+//     })
+//     callback();
+//   }
+// })
+
+
+
+const removeExtraEmptyElementTransformer = new stream.Transform({
+  objectMode: true,
+  flush(callback) {
+    if (this.ended) {
+      this.push(this.internalBuffer.join(''));
+    }
+    callback()
+  },
+  transform(chunk, enc, callback) {
+
+    try {
+      let obj
+      try {
+        const text = this.internalBuffer.join('\n') + chunk.toString()
+        obj = JSON.parse(text);
+        this.push(obj)
+      }
+      catch (e) {
+        // console.error(e);
+        this.internalBuffer.push(chunk)
+      }
+      // debug('removeExtraEmptyElementTransformer', '\nremoveExtraEmptyElementTransformer: typeof chunk=' + typeof chunk)
+
+      // debug('removeExtraEmptyElementTransformer', '\nremoveExtraEmptyElementTransformer: chunk=' + chunk.toString() + '<<<\n\n')
+
+      // debug('removeExtraEmptyElementTransformer', '\nremoveExtraEmptyElementTransformer: text=' + text + '<<<\n\n')
+
+      // const regex = /,\s*"\s*"/g
+      // if (regex.test(text)) {
+      //   const that = text.replaceAll(regex, ']')
+      //   debug('removeExtraEmptyElementTransformer', '\nremoveExtraEmptyElementTransformer: converting=>>>\n' + text + '<<<\nto\n>>>' + that)
+      //   this.push(that)
+      //   this.internalBuffer = []
+      // }
+      // else {
+      //   this.internalBuffer.push(chunk)
+      // }
+
+
+      callback();
+    } catch (e) {
+      console.error('\n\removeExtraEmptyElementTransformer - chunk=', chunk.toString())
+      callback(e);
+    }
+  }
+})
+removeExtraEmptyElementTransformer.flip = true;
+removeExtraEmptyElementTransformer.internalBuffer = []
+
+
+const removeExtraEmptyElementTransformer2 = new stream.Transform({
+  objectMode: true,
+  flush(callback) {
+    if (this.ended) {
+      this.push(this.internalBuffer.join(''));
+    }
+    callback()
+  },
+  transform(chunk, enc, callback) {
+
+    try {
+      debug('removeExtraEmptyElementTransformer', '\nremoveExtraEmptyElementTransformer2: chunk=' + chunk)
+      debug('removeExtraEmptyElementTransformer', '\nremoveExtraEmptyElementTransformer2: typeof chunk=' + typeof chunk)
+
+      if (util.isObject(chunk) && !Array.isArray(chunk)) {
+        for (const key in chunk) {
+          if (Object.hasOwnProperty.call(chunk, key)) {
+            const element = chunk[key];
+            debug('removeExtraEmptyElementTransformer', '\nelement: ' + key + '=' + element)
+          }
+        }
+      }
+
+      debug('removeExtraEmptyElementTransformer', '\nremoveExtraEmptyElementTransformer2: Json.stringify(chunk)=' + JSON.stringify(chunk))
+
+      function printit(obj, indent) {
+        let a = []
+        if (Array.isArray(obj)) {
+          obj.forEach(element => {
+            a.push(printit(element, indent + 1))
+          });
+        }
+        else {
+          a.push(''.padStart(' ', indent * 2) + obj)
+        }
+        return a
+      }
+      const ret = printit(chunk, 0);
+
+      this.push(ret)
+
+      callback();
+    } catch (e) {
+      console.error('\n\nremoveExtraEmptyElementTransformer - chunk=', chunk.toString())
+      callback(e);
+    }
+  }
+})
+removeExtraEmptyElementTransformer2.flip = true;
+removeExtraEmptyElementTransformer2.internalBuffer = []
+
+const directiveTransformer = new stream.Transform({
   flush(callback) {
     this.push(this.tokens)
     callback()
   },
   transform(chunk, enc, callback) {
-    if (!this.hasOwnProperty('tokens')) {
-      this.tokens = []
+
+    try {
+      debug('directiveTransformer', 'directiveTransformer: chunk=' + chunk.toString())
+      const regex = /\s*(?<DIRECTIVE>(block|include))(?<THEREST>.*)/
+
+      const matches = chunk.toString().match(regex)
+      debug('directiveTransformer', '\ndirectiveTransformer: matches=', matches)
+
+      if (matches) {
+        debug('directiveTransformer', '\ndirectiveTransformer: matches.groups=', matches.groups)
+        if (matches.groups) {
+          this.push('{ ' + (matches.groups.DIRECTIVE ? 'directive: "' + matches.groups.DIRECTIVE + '", ' : '') + 'params: "' + matches.groups.THEREST + '"}, ')
+        }
+      }
+      else {
+        this.push(chunk)
+      }
+      callback();
+    } catch (e) {
+      console.error('\n\ndirectiveTransformer - chunk=', chunk.toString())
+      callback(e);
     }
-    chunk.forEach((c) => {
-    // for (var c of chunk.toString()) {
-      if (c == '[') {
-        this.tokens.push(']')
-      }
-      else if (c == '{') {
-        this.tokens.push('}')
-      }
-      else if (c == '}' || c == ']') {
-        this.tokens.pop();
-      }
-    })
-    callback();
   }
 })
 
 
+const fileWriter = fs.createWriteStream('temp.json')
 
 process.stdin
-.pipe(lineTransformer)
-.pipe(blankLineRemoverTransformer)
-  
+  .pipe(lineTransformer)
+
+  // TODO: figure out performance gains by not requiring line numbers
+  .pipe(blankLineRemoverTransformer)
+
   .pipe(indentTransformer)
   // .pipe(nestingTransformer)
   .pipe(simpleNestingTransformer)
-  // .pipe(closerTransformer)
-  .pipe(process.stdout);
+  // .pipe(removeExtraEmptyElementTransformer)
+  // .pipe(removeExtraEmptyElementTransformer2)
+  // .pipe(objectify(function (chunk, enc, cb) {
+  //   var s = chunk.toString()
+  //   this.push(s)
+  //   cb()
+  // }))
+  // .pipe(process.stdout);
+
+  .pipe(concat(function (str) {
+    const arr = JSON.parse(str)
+
+    // console.dir(arr)
+
+    let newarr = deepFilter(arr, notEmpty)
+
+    // console.log(newarr)
+
+    function analyzeLine(el) {
+
+      return parser.parse(el.trim() == '//' ? '' : el) // { line: el }
+    }
+
+    function convert(el) {
+      const newArr = []
+      for (let i = 0; i < el.length; i++) {
+        if (i > 0 && Array.isArray(el[i])) {
+          if (newArr[newArr.length - 1].children && newArr[newArr.length - 1].children.length > 0) {
+            console.log('new obj=' + util.inspect(convert(el[i]), false, 10, true));
+            console.log('back ref=' + util.inspect(newArr[newArr.length - 1].children, false, 10, true));
+            throw new Error()
+            //newArr[newArr.length - 1].children.push(convert(el[i]))
+          }
+          else {
+            newArr[newArr.length - 1].children = convert(el[i])
+          }
+        }
+        else {
+          newArr.push(analyzeLine(el[i]))
+        }
+      }
+      debug('convert', 'returning ' + util.inspect(newArr))
+      return newArr
+    }
+
+    const thisArr = convert(newarr)
+    console.log(util.inspect(thisArr, false, 10, true))
+
+    // const newerarr = []
+
+    // const anotherObj = { line: newarr[0] }
+    // if (next object is a string) {
+    //   newerarr.push(anotherObj)
+    // }
+    // else {
+    //   newObj.children = createObj2(newarr.splice(2))
+    // }
+    // // newerarr.push({ line: newarr[1], children: createObj2(newarr.splice(2)) })
+
+    // const newObj = { line: newarr[1] }
+    // newObj.children = createObj2(newarr.splice(2))
+    // newerarr.push(newObj)
+
+    // function createObj2(anotherArray) {
+    //   const anotherNewArray = []
+    //   const children = createObj2(anotherArray.splice(1));
+    //   anotherNewArray.push( { line: anotherArray[0], children: children } )
+    //   return anotherNewArray
+    // }
+
+
+    // console.log(util.inspect(newerarr, false, 10, true))
+
+    // console.log(util.inspect(toObjects([], newarr[0], 0), false, 10, true))
+
+    // function toObjects(previousObj, newObj, indent) {
+
+
+
+
+
+    //   return lineOrArr.map(line => {
+    //   // for (let i = 0; i < lineOrArr.length; i++) {
+    //     const unknown = createObj(line);
+
+    //     if (Array.isArray(unknown)) {
+    //       console.log('this is an array=' + unknown);
+    //     }
+    //     else {
+    //       console.log('this is NOT an array=' + unknown);
+    //     }
+    //   })
+
+    //   // if (util.isObject(parent)) {
+    //   //   parent.children.push(lineOrArr)
+    //   // }
+    //   // else {
+    //   //   parent.push(lineOrArr)
+    //   // }
+
+    //   // let obj = { children: [] }
+    //   // debug('toObjects', 'enter: indent=' + indent + ', lineOrArr=' + lineOrArr + ', type=' + typeof lineOrArr);
+    //   // const indentStr = ''.padStart(indent * 2, ' ')
+    //   // if (typeof lineOrArr == 'string') {
+    //   //   obj = createObj(lineOrArr)
+    //   // }
+    //   // else if (Array.isArray(lineOrArr)) {
+    //   //   for (let i = 0; i < lineOrArr.length; i++) {
+    //   //     obj.children.push(toObjects(lineOrArr[i], indent + 1))
+    //   //   }
+    //   // }
+    //   // else {
+    //   //   debug('toObjects', 'WTF is this? ' + typeof lineOrArr)
+    //   // }
+    //   // return obj
+    // }
+
+    function createObj(line) {
+      if (Array.isArray(line)) {
+        return line.forEach(createObj)
+      }
+      else {
+        return { line: line }
+      }
+    }
+
+    // for (let i = 0; i < newarr.length; i++) {
+    //   backToLines(newarr[i], 0);
+    // }
+
+    // function backToLines(lineOrArr, indent) {
+    //   debug('backToLines', 'enter: indent=' + indent + ', lineOrArr=' + lineOrArr + ', type=' + typeof lineOrArr);
+    //   const indentStr = ''.padStart(indent * 2, ' ')
+    //   if (typeof lineOrArr == 'string') {
+    //     console.log(indentStr + lineOrArr);
+    //   }
+    //   else if (Array.isArray(lineOrArr)) {
+    //     for (let i = 0; i < lineOrArr.length; i++) {
+    //       backToLines(lineOrArr[i], indent + 1);
+    //     }
+
+    //   }
+    //   else {
+    //     debug('backToLines', 'WTF is this? ' + typeof lineOrArr)
+    //   }
+    // }
+  }))
+
+
+//.pipe(fileWriter)
+
+
+// .addListener('finish', async function (err) {
+//   if (err) {
+//     console.error('failed', err);
+//   } else {
+//     console.log('completed');
+
+//     try {
+//       const data = await fs.promises.readFile('temp.json', 'utf8');
+
+//       let arr = JSON.parse(data)
+
+//       console.log(arr)
+
+//       let newarr = deepFilter(arr, notEmpty)
+
+//       console.log(newarr)
+//     }
+//     catch (e) {
+//       console.error(e)
+//     }
+
+//   }
+// })
+
+function notEmpty(value, prop, subject) {
+  var key;
+
+  if (Array.isArray(value)) {
+    return value.length > 0;
+  } else if (!!value && typeof value === 'object' && value.constructor === Object) {
+    for (key in value) {
+      return true;
+    }
+    return false;
+  } else if (typeof value === 'string') {
+    subject[prop] = value = value.trim();
+
+    return value.length > 0;
+  } else {
+    return value != null;
+  }
+}
+
+// fileWriter.end('This is the end\n');
+// .pipe(process.stdout);
 
 let debugContent = [];
 
 function debug(pkg, ...msgs) {
-  if (pkg != 'tagtransformer' 
+  if (
+    //pkg == 'directiveTransformer'
+    // pkg != 'tagtransformer' 
     // && pkg != 'simpleNestingTransformer'
-    && pkg != 'blankLineRemoverTransformer'
-      
-      ) {// && pkg != 'nestingTransformer') {
+    // && pkg != 'blankLineRemoverTransformer'
+    // pkg == 'removeExtraEmptyElementTransformer'
+    // pkg == 'convert'
+    false
+  ) {// && pkg != 'nestingTransformer') {
     debugContent.push(...msgs)
   }
 }
